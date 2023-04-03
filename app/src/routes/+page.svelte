3058ec469492c163ef8d3383a9ef5a32bbdad5f4
csandrisTurr <csaszarandras@turr.hu>
2023-04-03 08:45:45 +0200

    fooldal elkezdese

--- app/src/routes/+page.svelte
+++ app/src/routes/+page.svelte
@@ -1,7 +1,32 @@
 <main>
+    <section>
+        <div class="front">
+            <span>Projektmenedzsment emészthető módon</span>
+            <a href="#leirasok">Kezdjünk bele!</a>
+        </div>
+        <div class="back"></div>
+    </section>
+    <section id="leirasok">
+        <div></div>
+    </section>
+    <section>
+        
+    </section>
+    <section>
+        
+    </section>
 </main>
 
 <style lang="scss">
+    @import '../scss/palette.scss';
+
+    section {
+        height: 100vh;
+    }
+
+    section:nth-child(odd) {
+        background-color: $blue-0;
+    }
 </style>
 
 <script lang="ts">
