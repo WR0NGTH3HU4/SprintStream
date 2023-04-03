b2f4ced551c077593657506902dc351e81e96e8e
TatraiDominik <78853807+TatraiDominik@users.noreply.github.com>
2023-04-03 18:46:21 +0200

    navbar továbbfejlesztése

--- app/src/components/Navbar.svelte
+++ app/src/components/Navbar.svelte
@@ -1,19 +1,22 @@
 <main>
     
     <div class="cim">
-        <h3>SprintStream</h3>
+        <h1>SprintStream</h1>
     </div>
     <div class="modellek">
         <h3>Scrum</h3>
+        <img src="icons/down_arrow.png" alt="down_arrow">
     </div>
     <div class="modellek">
         <h3>Vízesés</h3>
+        <img src="icons/down_arrow.png" alt="down_arrow">
     </div>
     <div class="modellek">
         <h3>Rólunk</h3>
+        <img src="icons/down_arrow.png" alt="down_arrow">
     </div>
     <div class="gomb">
-        <img src="icons/gear.png">
+        <img src="icons/gear.png" alt="gear">
     </div>
 </main>
 <style>
@@ -27,5 +30,18 @@
         flex-direction: row;
         flex-wrap: nowrap;
         justify-content: space-between;
+        align-items: center;
+        margin-right: 20px;
+        margin-left: 20px;
+    }
+    .modellek{
+        display: flex;
+        flex-direction: row;
+        flex-wrap: nowrap;
+        justify-content: space-between;
+        align-items: center;
+    }
+    .modellek img{  
+        
     }
 </style>
