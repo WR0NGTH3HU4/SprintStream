2897d6bef669936b1a625fdefca70faa9cbbf5f8
Csóka András <80397154+csokaandras@users.noreply.github.com>
2023-04-03 22:06:37 +0200

    hullámok berakása

--- app/src/routes/quiz_oldal/+page.svelte
+++ app/src/routes/quiz_oldal/+page.svelte
@@ -1,7 +1,29 @@
 <main>
+    <!DOCTYPE html>
+    <html lang="hu">
+    <head>
+        <meta charset="UTF-8">
+        <meta http-equiv="X-UA-Compatible" content="IE=edge">
+        <meta name="viewport" content="width=device-width, initial-scale=1.0">
+        <title>Quiz</title>
+    </head>
+    <body>
+        <div class="kerdes">
+            <img src="wave_top.svg" alt="wave_top">
+            
+            <img src="wave_bottom.svg" alt="wave_bottom">
+        </div>
+        
+    </body>
+    </html>
+    
+    
 </main>
 
 <style lang="scss">
+    body{
+        margin: 0px;
+    }
 </style>
 
 <script lang="ts">
