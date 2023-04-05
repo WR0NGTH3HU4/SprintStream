a92cbcf4f7ea2dbf9d15efdfac30cfed398bc3f3
csandrisTurr <csaszarandras@turr.hu>
2023-04-05 09:16:36 +0200

    valami

---
+++ app/src/routes/+layout.svelte
@@ -0,0 +1,13 @@
+<style lang="scss">
+    @import url('https://fonts.googleapis.com/css2?family=Inter:wght@100;200;300;400;500;600;700;800;900&display=swap');
+
+    :global(*) {
+        padding: 0;
+        margin: 0;
+    }
+
+    :global(html, body) {
+        width: 100vw;
+        min-height: 100vh;
+    }
+</style>
