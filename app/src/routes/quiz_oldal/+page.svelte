154f5db258c64c14d2e2ee7d98d34c367b539b3d
Csoki <csokaandras@turr.hu>
2023-04-05 11:58:07 +0200

    Kérdés kártyák méretezése elrendezése

--- app/src/routes/quiz_oldal/+page.svelte
+++ app/src/routes/quiz_oldal/+page.svelte
@@ -1,30 +1,70 @@
 <main>
-    <div class="kerdes">
+    <div class="card platina">
         <img src="wave_top.svg" alt="wave_top">
-        <p>Lorem ipsum dolor, sit amet consectetur adipisicing elit. A saepe optio nemo eos aperiam iusto id, deserunt magni praesentium accusamus unde quo laudantium placeat veniam tempore. Est laborum tempore fugiat.</p>
+        <div class="kerdes">
+            <p>Lorem ipsum dolor, sit amet consectetur adipisicing elit. A saepe optio nemo eos aperiam iusto id, deserunt magni praesentium accusamus unde quo laudantium placeat veniam tempore. Est laborum tempore fugiat.</p>
+        </div>
         <img src="wave_bottom.svg" alt="wave_bottom">
     </div>
 
-    <div class="kerdes">
+    <div class="card kek">
+        
+        <div class="kerdes">
+            <p>Lorem ipsum dolor, sit amet consectetur adipisicing elit. A saepe optio nemo eos aperiam iusto id, deserunt magni praesentium accusamus unde quo laudantium placeat veniam tempore. Est laborum tempore fugiat.</p>
+        </div>
+        
+    </div>
+
+    <div class="card platina">
         <img src="wave_top.svg" alt="wave_top">
-        <p>Lorem ipsum dolor, sit amet consectetur adipisicing elit. A saepe optio nemo eos aperiam iusto id, deserunt magni praesentium accusamus unde quo laudantium placeat veniam tempore. Est laborum tempore fugiat.</p>
+        <div class="kerdes">
+            <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Dolorum, dignissimos? Atque, quibusdam odit ab perspiciatis temporibus ipsam ullam deleniti asperiores voluptas mollitia? Aspernatur, facere. Nulla, sapiente! Fugit delectus debitis inventore.</p>            
+        </div>
+        
         <img src="wave_bottom.svg" alt="wave_bottom">
     </div>
     
 </main>
 
 <style lang="scss">
-    .kerdes img{
+    main{
+        display: flex;
+        flex-direction: column;
+    }
+
+    .card{
+        display: flex;
+        flex-direction: column;
+        justify-content: space-between;
+    }
+
+    .card img{
         width: 100vw;
         height: auto;
     }
 
-    .kerdes{
+    .card{
         margin: 0px;
         padding: 0px;
         width: 100vw;
-        height: auto;
+        height: 100vh;
+    }
+
+    .platina{
         background-color: #E2E2E2;
+    }
+
+    .kek{
+        background-color: #5465FF;
+        display: flex;
+        flex-direction: column;
+        justify-content: center;
+    }
+
+    .kerdes{
+        display: flex;
+        flex-direction: row;
+        justify-content: center;
     }
 
 </style>
