a21c000baf29b16b94dd757219a15a1599d126fd
csandrisTurr <csaszarandras@turr.hu>
2023-04-12 09:41:02 +0200

    poziciok javitasa

--- app/src/routes/+page.svelte
+++ app/src/routes/+page.svelte
@@ -18,25 +18,23 @@
                 <span>Olvass bele az egyszerű leírásainkba!</span>
                 <span>Gondosan átnéztünk számtalan hivatalos dokumentumot a Scrum és a Vízesés-modell specifikációiról, hogy azokat leegyszerűsítsük felhasználóink számára a zökkenőmentes tanulás érdekében.</span>
             </div>
-            <div class="feature">
-                <a href="" class="button">Jól hangzik, vigyél oda!</a>
-            </div>
+            <a href="" class="button">Jól hangzik, vigyél oda!</a>
         </div>
         <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 1440 320">
             <path fill="#788BFF" fill-opacity="1" d="M0,256L30,218.7C60,181,120,107,180,117.3C240,128,300,224,360,266.7C420,309,480,299,540,245.3C600,192,660,96,720,85.3C780,75,840,149,900,149.3C960,149,1020,75,1080,48C1140,21,1200,43,1260,74.7C1320,107,1380,149,1410,170.7L1440,192L1440,320L1410,320C1380,320,1320,320,1260,320C1200,320,1140,320,1080,320C1020,320,960,320,900,320C840,320,780,320,720,320C660,320,600,320,540,320C480,320,420,320,360,320C300,320,240,320,180,320C120,320,60,320,30,320L0,320Z"></path>
             <path fill="#5465FF" fill-opacity="1" d="M0,96L30,101.3C60,107,120,117,180,133.3C240,149,300,171,360,192C420,213,480,235,540,224C600,213,660,171,720,160C780,149,840,171,900,176C960,181,1020,171,1080,144C1140,117,1200,75,1260,53.3C1320,32,1380,32,1410,32L1440,32L1440,320L1410,320C1380,320,1320,320,1260,320C1200,320,1140,320,1080,320C1020,320,960,320,900,320C840,320,780,320,720,320C660,320,600,320,540,320C480,320,420,320,360,320C300,320,240,320,180,320C120,320,60,320,30,320L0,320Z"></path>
         </svg>
     </section>
-    <section>
+    <section id="teszt">
         <div class="feature">
             <span>Teszteld tudásod a kvízeinkkel!</span>
             <span>Nem vagy biztos abban, hogy minden fontos részét megtanultad az anyagnak? Mérd fel tudásod a saját kvízeinkkel! Mi csak olyat kérdezünk, ami biztos fontos és le lett írva :)</span>
         </div>
-        <div class="feature">
+        <div id="question-wrapper">
             <img src="question.svg" alt="">
         </div>
     </section>
-    <section>
+    <section id="veg">
         <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 1440 320">
             <path fill="#788BFF" fill-opacity="1" d="M0,64L40,53.3C80,43,160,21,240,42.7C320,64,400,128,480,144C560,160,640,128,720,138.7C800,149,880,203,960,197.3C1040,192,1120,128,1200,117.3C1280,107,1360,149,1400,170.7L1440,192L1440,0L1400,0C1360,0,1280,0,1200,0C1120,0,1040,0,960,0C880,0,800,0,720,0C640,0,560,0,480,0C400,0,320,0,240,0C160,0,80,0,40,0L0,0Z"></path>
             <path fill="#5465FF" fill-opacity="1" d="M0,160L40,149.3C80,139,160,117,240,117.3C320,117,400,139,480,128C560,117,640,75,720,69.3C800,64,880,96,960,122.7C1040,149,1120,171,1200,165.3C1280,160,1360,128,1400,112L1440,96L1440,0L1400,0C1360,0,1280,0,1200,0C1120,0,1040,0,960,0C880,0,800,0,720,0C640,0,560,0,480,0C400,0,320,0,240,0C160,0,80,0,40,0L0,0Z"></path>
@@ -44,7 +42,7 @@
         <div>
             <div class="feature">
                 <span>Mindezt teljesen ingyen!</span>
-                <span>Az oldal teljesen nonprofit, nem kell regisztrálni a használatához. Emellett az egész projekt nyílt forráskódú és minden része elérhető a <a href="">GitHub</a> reponkban.</span>
+                <span>Az oldal teljesen nonprofit, nem kell regisztrálni a használatához. Emellett az egész projekt nyílt forráskódú és minden része elérhető a <a href="https://github.com/WR0NGTH3HU4/SprintStream">GitHub</a> reponkban.</span>
             </div>
         </div>
         <div></div>
@@ -89,7 +87,7 @@
         display: flex;
         flex-direction: column;
         justify-content: center;
-        align-items: center;
+        align-items: flex-start;
 
         span:nth-child(1) {
             font-size: 2rem;
@@ -97,7 +95,21 @@
         }
     }
 
-    section:nth-child(2) > div, section:nth-child(3) {
+    #leirasok > div {
+        justify-content: space-evenly;
+        align-items: center;
+
+        & > * {
+            flex-grow: 0;   
+        }
+
+        .button {
+            max-width: 10rem;
+            max-height: 4rem;
+        }
+    }
+
+    #leirasok > div {
         display: flex;
         flex-direction: row;
         padding: 0 5rem;
@@ -105,9 +117,22 @@
         & > * {
             flex-basis: 50%;
         }
-        
-        & > div {
-            & > img { // csak hogy ne nezzen ki hulyen a kerdojel
+    }
+
+    #teszt {
+        flex-direction: row;
+        justify-content: space-evenly;
+
+        & > * {
+            flex-basis: 40%;
+        }
+
+        #question-wrapper {
+            display: flex;
+            flex-direction: column;
+            justify-content: center;
+
+            & > img {
                 width: 100%;
             }
         }
@@ -135,6 +160,7 @@
 
         & > img {
             animation: linear infinite spin 25s;
+            height: 60vw;
         }
     }
 
