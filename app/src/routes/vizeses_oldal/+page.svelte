a2e0bc1a5bc9cee27a50a59eed22cc18fc4e97e4
TatraiDominik <78853807+TatraiDominik@users.noreply.github.com>
2023-04-12 21:19:16 +0200

    körök pozicionálása + nézetek elkezdése

--- app/src/routes/vizeses_oldal/+page.svelte
+++ app/src/routes/vizeses_oldal/+page.svelte
@@ -29,6 +29,13 @@
             </div>
 
         </div>
+        <div class="ph">
+            <svg width="274" height="243" viewBox="0 0 274 243" fill="none" xmlns="http://www.w3.org/2000/svg">
+                <path d="M119.68 9.99995C127.378 -3.33338 146.622 -3.33334 154.321 10L271.234 212.5C278.932 225.833 269.309 242.5 253.913 242.5H20.0865C4.69051 242.5 -4.93194 225.833 2.76606 212.5L119.68 9.99995Z" fill="#BFD7FF"/>
+                <path d="M138 88C102.185 88 73 117.185 73 153C73 188.815 102.185 218 138 218C173.815 218 203 188.815 203 153C203 117.185 173.815 88 138 88ZM169.07 138.05L132.215 174.905C131.305 175.815 130.07 176.335 128.77 176.335C127.47 176.335 126.235 175.815 125.325 174.905L106.93 156.51C105.045 154.625 105.045 151.505 106.93 149.62C108.815 147.735 111.935 147.735 113.82 149.62L128.77 164.57L162.18 131.16C164.065 129.275 167.185 129.275 169.07 131.16C170.955 133.045 170.955 136.1 169.07 138.05Z" fill="#5465FF"/>
+                </svg>
+                
+        </div>
         <div class="tartalom">
             <svg class="elsohullam" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 1440 320">
                 <path  fill="#BFD7FF" fill-opacity="1" d="M0,288L48,261.3C96,235,192,181,288,181.3C384,181,480,235,576,229.3C672,224,768,160,864,154.7C960,149,1056,203,1152,234.7C1248,267,1344,277,1392,282.7L1440,288L1440,0L1392,0C1344,0,1248,0,1152,0C1056,0,960,0,864,0C768,0,672,0,576,0C480,0,384,0,288,0C192,0,96,0,48,0L0,0Z"></path>   
@@ -41,9 +48,9 @@
                     <h1>Üdv az első lépésben</h1>
                     <p>szovegs</p>
                 </div>
-                <div>
+                
                     <img src="icons/document.png" alt="kep">
-                </div>
+                
             </div>
                 <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 1440 320">
 
@@ -53,16 +60,24 @@
 
                     <path fill="#5465FF" fill-opacity="1" d="M0,64L26.7,106.7C53.3,149,107,235,160,266.7C213.3,299,267,277,320,250.7C373.3,224,427,192,480,186.7C533.3,181,587,203,640,208C693.3,213,747,203,800,181.3C853.3,160,907,128,960,144C1013.3,160,1067,224,1120,234.7C1173.3,245,1227,203,1280,160C1333.3,117,1387,75,1413,53.3L1440,32L1440,320L1413.3,320C1386.7,320,1333,320,1280,320C1226.7,320,1173,320,1120,320C1066.7,320,1013,320,960,320C906.7,320,853,320,800,320C746.7,320,693,320,640,320C586.7,320,533,320,480,320C426.7,320,373,320,320,320C266.7,320,213,320,160,320C106.7,320,53,320,27,320L0,320Z"></path>
                 </svg>
-                <div class="fill">
-                    <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 1440 320">
-                        <path fill="#000000" fill-opacity="1" d="M0,64L34.3,64C68.6,64,137,64,206,90.7C274.3,117,343,171,411,165.3C480,160,549,96,617,106.7C685.7,117,754,203,823,218.7C891.4,235,960,181,1029,165.3C1097.1,149,1166,171,1234,181.3C1302.9,192,1371,192,1406,192L1440,192L1440,320L1405.7,320C1371.4,320,1303,320,1234,320C1165.7,320,1097,320,1029,320C960,320,891,320,823,320C754.3,320,686,320,617,320C548.6,320,480,320,411,320C342.9,320,274,320,206,320C137.1,320,69,320,34,320L0,320Z"></path>
-                      </svg>
-                </div>
+
+        </div>
+        <div class="fill">
+            <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 1440 320">
+                <path fill="#000000" fill-opacity="1" d="M0,64L34.3,64C68.6,64,137,64,206,90.7C274.3,117,343,171,411,165.3C480,160,549,96,617,106.7C685.7,117,754,203,823,218.7C891.4,235,960,181,1029,165.3C1097.1,149,1166,171,1234,181.3C1302.9,192,1371,192,1406,192L1440,192L1440,320L1405.7,320C1371.4,320,1303,320,1234,320C1165.7,320,1097,320,1029,320C960,320,891,320,823,320C754.3,320,686,320,617,320C548.6,320,480,320,411,320C342.9,320,274,320,206,320C137.1,320,69,320,34,320L0,320Z"></path>
+              </svg>
         </div>
     </div>
 </main>
 
 <style lang="scss">
+    .main{
+        border-color: #5465FF;
+        display: flex;
+        flex-direction: row;
+        justify-content: center;
+        align-items: center;
+    }
 
     .egesz{
         width: 100%;
@@ -73,18 +88,22 @@
         height: 70rem;
         display: flex;
         flex-direction: row;
+        flex-wrap: wrap ;
         justify-content:center;
         align-items: center;
+        align-content: space-between;
         position:relative;
         
     }
     .cim{
+
         width:100%;
         display: flex;
         flex-direction: row;
         justify-content:space-evenly ;
         align-items: center;
         padding-bottom: 20%;
+        padding-top: 10%;
     }
     .cim h1{
         font-size: 15rem;
@@ -94,74 +113,73 @@
         flex-direction: column;
         justify-content: center;
         align-items: center ;
-        flex-wrap: wrap;
-        position: absolute; 
+
+        position: absolute;
         border-radius: 100%;
         text-align: center;
+        font-size: 150%;
     }
     .kor1{
         
         background-color:#BFD7FF ;
-        width: 20rem;
-        height: 20rem;
-        top:5%;
-        right: 67%;
-        z-index: 4;
-        
+        width: 30rem;
+        height: 30rem;
+
     }
     .kor2{
         background-color:#788BFF ;
-        width: 18rem;
-        height: 18rem;
-        top:-3%;
-        right: 52%;
-        z-index: 5;
+        width: 25rem;
+        height: 25rem;
+
     }
     .kor3{
         
         background-color:#9BB1FF ;
-        width: 22rem;
-        height: 22rem;
-        top:18%;
-        right: 47%;
-        z-index: 3;
+        width: 32rem;
+        height: 32rem;
+
     }
     .kor4{
         background-color:#E3E3E3 ;
-        width: 15rem;
-        height: 15rem;
-        top:18%;
-        right: 34%;
+        width: 25rem;
+        height: 25rem;
+
         
         
-        z-index: 2;
+
     }
     .kor5{
         background-color:#BFD7FF ;
-        width: 22rem;
-        height: 22rem;
-        top:3%;
-        right: 12%;
-        
-        z-index: 2;
+        width: 32rem;
+        height: 32rem;
+
     }
     .kor6{
         background-color:#788BFF ;
-        width: 25rem;
-        height: 25rem;
-        top:25%;
-        right: 12%;
-        
-        z-index: 1;
+        width: 35rem;
+        height: 35rem;
+
     }
     .kor7{
         background-color:#BFD7FF ;
-        width: 15rem;
-        height: 15rem;
-        top:43%;
-        right: 40%;
-        
-        z-index: 0;
+        width: 25rem;
+        height: 25rem;
+
+    }
+    .ph{
+        background-color: #788BFF;
+        width: 23rem;
+        height: 23rem;
+        border-radius: 100%;
+        display: flex;
+        flex-direction: column;
+        flex-wrap: nowrap;
+        justify-content: center;
+        align-items: center;
+        margin-left: 10%;
+    }
+    .ph svg{
+        transform: translate(0%,-10%);
     }
     .tartalom{
         display: flex;
@@ -181,18 +199,25 @@
         z-index: 0;
     }
     .szoveg{
-        margin-top:3%;
-        margin-bottom: 3%;
+        width: 100%;
         display: flex;
         flex-direction: row;
         justify-content: center;
         align-content: space-between;
-        align-items: center;
-        flex-wrap: wrap;
+
+       
+    }
+    .szoveg div{
+        align-self:flex-start ;
+        flex-grow: 2;
+    }
+    .szoveg img{
+        align-self:flex-end ;
+       
     }
     .fill{
         height: 40rem;
-        width: 100vw;
+        width: 100%;
         background-color: #5465FF;
         display: flex;
         flex-direction: column;
@@ -200,9 +225,166 @@
         
     }
     .fill svg{
-        width: 100vw;
+        width: 100%;
+        display: block;
+
+    }
+    @media screen and (min-width: 750px){
+        .main{
+        
+        height: 70rem;
+        display: flex;
+        flex-direction: row;
+        justify-content:center;
+        align-items: center;
+        position:relative;
+        
+    }
+.kor{
+        display: flex;
+        flex-direction: column;
+        justify-content: center;
+        align-items: center ;
+        flex-wrap: wrap;
+        position: absolute; 
+        border-radius: 100%;
+        text-align: center;
+        font-size: 150%;
+    }
+    .kor1{
+        
+        background-color:#BFD7FF ;
+        width: 30rem;
+        height: 30rem;
+        top:5%;
+        right: 70%;
+        z-index: 4;
+        
+    }
+    .kor2{
+        background-color:#788BFF ;
+        width: 25rem;
+        height: 25rem;
+        top:-3%;
+        right: 52%;
+        z-index: 5;
+    }
+    .kor3{
+        
+        background-color:#9BB1FF ;
+        width: 32rem;
+        height: 32rem;
+        top:18%;
+        right: 47%;
+        z-index: 3;
+    }
+    .kor4{
+        background-color:#E3E3E3 ;
+        width: 25rem;
+        height: 25rem;
+        top:18%;
+        right: 31%;
+        
+        
+        z-index: 2;
+    }
+    .kor5{
+        background-color:#BFD7FF ;
+        width: 32rem;
+        height: 32rem;
+        top:3%;
+        right: 9%;
+        
+        z-index: 2;
+    }
+    .kor6{
+        background-color:#788BFF ;
+        width: 35rem;
+        height: 35rem;
+        top:35%;
+        right: 12%;
+        
+        z-index: 1;
+    }
+    .kor7{
+        background-color:#BFD7FF ;
+        width: 25rem;
+        height: 25rem;
+        top:52%;
+        right: 40%;
+        
+        z-index: 0;
     }
 
+    }
+    @media screen and (max-width: 1840px){
+        .kor{
+            transform: scale(.9) translate(15%,0%);
+            
+        }
+    }
+    @media screen and (max-width: 1580px){
+        .kor{
+            transform: scale(.8) translate(30%,0%);
+        }
+    }
+    @media screen and (max-width: 1420px){
+        .kor{
+            transform: scale(.7) translate(30%,0%);
+        }
+    }
+    @media screen and (max-width: 1120px){
+        .kor{
+            transform: scale(.6) translate(40%,0%);
+            font-size: 200%;
+        }
+    }
+    @media screen and (max-width: 1000px){
+        .kor{
+            transform: scale(.55) translate(45%,0%);
+            font-size: 200%;
+        }
+    }
+    @media screen and (max-width: 950px){
+        .kor{
+            transform: scale(.55) translate(55%,0%);
+            font-size: 200%;
+        }
+    }
+    @media screen and (max-width: 825px){
+        .kor{
+            transform: scale(.50) translate(58%,0%);
+            font-size: 200%;
+        }
+    }
+    @media screen and (max-width: 810px){
+        .kor{
+            transform: scale(.50) translate(63%,0%);
+            font-size: 200%;
+        }
+    }
+    @media screen and (max-width: 760px){
+        .kor{
+            transform: scale(.450) translate(66%,0%);
+            font-size: 200%;
+        }
+    }
+    @media screen and (max-width: 750px){
+        .kor{
+            font-size: 200%;
+            top:none;
+            right: none;
+            bottom: none;
+            left:none;
+            
+        }
+        .main{
+            position: relative;
+            display: flex;
+            flex-direction:row;
+
+        }
+    }
 </style>
 
 <script lang="ts">
