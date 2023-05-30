47ad30975b2c69711eec6bea0e09ea7e6cb838f1
csandrisTurr <csaszarandras@turr.hu>
2023-05-30 21:37:23 +0200

    athelyezem a cuccokat masik branchrol

---
+++ app/src/routes/rolunk/+page.svelte
@@ -0,0 +1,105 @@
+<main>
+    <section class="main">
+        <ClickerGenerator elemShape="circle" shapes="{shapes}" />
+    </section>
+</main>
+
+<style lang="scss">
+    @import '../../scss/palette.scss';
+
+    section {
+        display: flex;
+        flex-direction: column;
+        height: 100vh;
+    }
+
+    section:nth-child(odd) {
+        background-color: $blue-0;
+        color: white;
+        padding: 1rem;
+    }
+
+    section:nth-child(even) {
+        justify-content: space-between;
+
+        & > div {
+            padding: 1rem;
+        }
+    }
+</style>
+
+<script lang="ts">
+	import ClickerGenerator from "../../components/ClickerGenerator.svelte";
+	import Navbar from "../../components/Navbar.svelte";
+    import { onMount } from 'svelte';
+    
+    const shapes = [
+        {
+            text: ""
+        },
+        {
+            text: ""
+        },
+        {
+            text: ""
+        }
+    ];
+
+    const rolunk = [
+        {
+            nev: "Csóka András",
+            leiras: "",
+            kep: "",
+            media: [
+                {
+                    tipus: "github",
+                    link: ""
+                }
+            ],
+        },
+        {
+            nev: "Császár András",
+            leiras: "",
+            kep: "",
+            media: [
+                {
+                    tipus: "github",
+                    link: ""
+                }
+            ],
+        },
+        {
+            nev: "Kővágó Levente",
+            leiras: "",
+            kep: "",
+            media: [
+                {
+                    tipus: "github",
+                    link: ""
+                }
+            ],
+        },
+        {
+            nev: "Kulimák Máté",
+            leiras: "",
+            kep: "",
+            media: [
+                {
+                    tipus: "github",
+                    link: ""
+                }
+            ],
+        },
+        {
+            nev: "Tátrai Dominik Oszkár",
+            leiras: "",
+            kep: "",
+            media: [
+                {
+                    tipus: "github",
+                    link: ""
+                }
+            ],
+        },
+    ];
+</script>
